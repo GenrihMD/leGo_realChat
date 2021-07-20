3f80388f571540b20e9ee74fb15a4b59d214c302
Лямин <genrih.md@gmail.com>
2021-07-20 09:58:57 +0300

    Add types and manager

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
 	id     string
 	socket *websocket.Conn
 	send   chan []byte
+}
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+}
+
+var manager = ClientManager{
+	broadcast:  make(chan []byte),
+	register:   make(chan *Client),
+	unregister: make(chan *Client),
+	clients:    make(map[*Client]bool),
 }
 
 func main() {
