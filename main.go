023389d358474500eb4a0f9248f330f623a507f0
Лямин <genrih.md@gmail.com>
2021-07-22 12:05:20 +0300

    Add st methods

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	uuid "github.com/satori/go.uuid"
 )
 
 type Client struct {
@@ -57,6 +58,42 @@ type ClientManager struct {
 	unregister chan *Client
 }
 
+func (m *ClientManager) start() {
+	for {
+		select {
+		case conn := <-m.register:
+			m.clients[conn] = true
+			jsonMessage, _ := json.Marshal(&Message{Content: "/A new socket has connected."})
+			m.send(jsonMessage, conn)
+
+		case conn := <-m.unregister:
+			if _, ok := m.clients[conn]; ok {
+				close(conn.send)
+				delete(m.clients, conn)
+				jsonMessage, _ := json.Marshal(&Message{Content: "/A socket has disconnected"})
+				m.send(jsonMessage, conn)
+			}
+		case message := <-m.broadcast:
+			for conn := range m.clients {
+				select {
+				case conn.send <- message:
+				default:
+					close(conn.send)
+					delete(m.clients, conn)
+				}
+			}
+		}
+	}
+}
+
+func (m *ClientManager) send(message []byte, ignore *Client) {
+	for conn := range m.clients {
+		if conn != ignore {
+			conn.send <- message
+		}
+	}
+}
+
 type Message struct {
 	Sender    string `json:"sender,omitempty"`
 	Recipient string `json:"recipient,omitempty"`
@@ -71,7 +108,24 @@ var manager = ClientManager{
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
+	u := &websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
+	conn, err := u.Upgrade(w, r, nil)
 
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	client := &Client{
+		id:     uuid.NewV4().String(),
+		socket: conn,
+		send:   make(chan []byte),
+	}
+
+	manager.register <- client
+
+	go client.read()
+	go client.write()
 }
 
 func main() {
